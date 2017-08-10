a426e4bc8f7a0f90ef030772e939971e3b30b865
antonio.filho <antonio.filho@castgroup.com.br>
2017-08-10 13:15:38 -0300

    Classe de lancamento

---
+++ src/main/java/com/algaworks/algamoneyapi/algamoneyapi/model/Lancamento.java
@@ -0,0 +1,27 @@
+package com.algaworks.algamoneyapi.algamoneyapi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "lancamento")
+public class Lancamento {
+
+    private Long codigo;
+    private String descricao;
+
+    @Column(name = "data_vencimento")
+    private LocalDate dataVencimento;
+
+    @Column(name = "data_pagamento")
+    private LocalDate dataPagamento;
+
+    private BigDecimal valor;
+
+    private String observacao;
+
+    private TipoLancamento tipoLancamento;
+}
