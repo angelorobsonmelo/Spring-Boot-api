eb9864d36b605de0a2c13bbb80d6eeab9a7153d8
Ângelo Robson <angelorobsonmelo@gmail.com>
2017-09-07 13:55:20 -0300

    Profiles

--- src/main/java/com/algaworks/algamoneyapi/algamoneyapi/cors/CorsFilter.java
+++ src/main/java/com/algaworks/algamoneyapi/algamoneyapi/cors/CorsFilter.java
@@ -12,6 +12,8 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.algaworks.algamoneyapi.algamoneyapi.config.property.AlgaMoneyApiProperty;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -20,7 +22,8 @@ import org.springframework.stereotype.Component;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-  private String originPermitida = "http://localhost:63342"; // TODO: Configurar para diferentes ambientes
+  @Autowired
+  private AlgaMoneyApiProperty algaMoneyApiProperty;
 
   @Override
   public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
@@ -29,10 +32,10 @@ public class CorsFilter implements Filter {
     HttpServletRequest request = (HttpServletRequest) req;
     HttpServletResponse response = (HttpServletResponse) resp;
 
-    response.setHeader("Access-Control-Allow-Origin", originPermitida);
+    response.setHeader("Access-Control-Allow-Origin", algaMoneyApiProperty.getOriginPermitida());
     response.setHeader("Access-Control-Allow-Credentials", "true");
 
-    if ("OPTIONS".equals(request.getMethod()) && originPermitida.equals(request.getHeader("Origin"))) {
+    if ("OPTIONS".equals(request.getMethod()) && algaMoneyApiProperty.getOriginPermitida().equals(request.getHeader("Origin"))) {
       response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
       response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
       response.setHeader("Access-Control-Max-Age", "3600");
